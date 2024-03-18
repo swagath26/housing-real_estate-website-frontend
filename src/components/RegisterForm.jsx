2a322515f176171969b44aeb6d58f2b2a4501e5b
Swagath N S <swagathns2000@gmail.com>
2024-03-18 05:51:29 +0530

    accounts section form-validation done

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,16 +1,10 @@
-import { useState, useContext } from "react"
-import { FaLock, FaUser } from "react-icons/fa";
+import { useState, useContext, useEffect, useRef } from "react"
+import { FaLock, FaUser, FaKey, FaIdCard } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-// import getCookie from "./csrfToken";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import AuthContext from "./AuthContext";
 
-// const csrftoken = getCookie('csrftoken');
-
 const RegisterForm = () => {
-
-    const navigate = useNavigate();
 
     const setIsAuthenticated = useContext(AuthContext).setIsAuthenticated;
     const csrftoken = useContext(AuthContext).csrftoken;
@@ -23,11 +17,45 @@ const RegisterForm = () => {
     const [password2, setPassword2] = useState('');
     const [rememberme, setRememberme] = useState(false);
 
-    const [errorMessage, setErrorMessage] = useState(false);
-    // const [successMessage, setSuccessMessage] = useState(null);
+    const [errors, setErrors] = useState(null);
     
-    const handleRegister = async (event) => {
-        event.preventDefault();
+    useEffect(() => {
+        const handleError = () => {
+            if(errors) {
+                errors.email && errors.email.forEach((error, index) => {
+                    console.log(index, error);
+                })
+                errors.username && errors.username.forEach((error, index) => {
+                    console.log(index, error);
+                })
+                errors.password1 && errors.password1.forEach((error, index) => {
+                    console.log(index, error);
+                })
+                errors.password2 && errors.password2.forEach((error, index) => {
+                    console.log(index, error);
+                })
+            }
+        }
+        handleError();
+    }, [errors]);
+
+    const clearFields = () => {
+        setEmail('');
+        setUsername('');
+        setFirstName('');
+        setlastName('');
+        setPassword1('');
+        setPassword2('');
+        setRememberme(false);
+        setErrors(null);
+        setFormValidate(false);
+        setIsFormValidated(false);
+        ['email', 'username', 'first_name', 'last_name', 'password1', 'password2'].forEach((input) => {
+            document.getElementById(input).classList.remove('is-valid');
+        });
+    }
+
+    const handleRegister = async () => {
         const formData = new FormData();
         formData.append('email', email);
         formData.append('first_name', firstName);
@@ -36,32 +64,143 @@ const RegisterForm = () => {
         formData.append('password1', password1);
         formData.append('password2', password2);
         formData.append('remember_me', rememberme);
-        
-        // for (const [key, value] of formData.entries()) {
-        //     console.log(`${key}: ${value}`);
-        // }
 
         const response = await axios.post('/api/members/signup/', formData, {headers: { 'X-CSRFToken': csrftoken }});
-        console.log(response);
         if (response.data.success) {
-            // setSuccessMessage(response.data.messages);
-            // setErrorMessage(false);
+            clearFields();
             window.bootstrap.Modal.getInstance('#accounts').hide();
             setIsAuthenticated(true);
         }
         else {
-            setErrorMessage(true);
+            setErrors(response.data.errors);
             setIsAuthenticated(false);
-            // console.log(response.data.errors);
-            // console.log(errorMessage)
         }
     }
+
+    const [formValidate, setFormValidate] = useState(false);
+    const [isFormValidated, setIsFormValidated] = useState(false);
+    const [isPassword1Validated, setIsPassword1Validated] = useState(false);
+    const [isPassword2Validated, setIsPassword2Validated] = useState(false);
+    const [isEmailValidated, setIsEmailValidated] = useState(false);
+    const [isFirstNameValidated, setIsFirstNameValidated] = useState(false);
+    const [isLastNameValidated, setIsLastNameValidated] = useState(false);
+    const [isUsernameValidated, setIsUsernameValidated] = useState(false);
+
+    const validateField = (input) => {
+        !input.classList.contains('is-valid') && input.classList.add('is-valid');
+        input.classList.contains('is-invalid') && input.classList.remove('is-invalid');
+    }
+
+    const invalidateField = (input) => {
+        !input.classList.contains('is-invalid') && input.classList.add('is-invalid');
+        input.classList.contains('is-valid') && input.classList.remove('is-valid');
+    }
+
+    useEffect(() => {
+        if(isPassword1Validated && isPassword2Validated && isEmailValidated && isFirstNameValidated && isLastNameValidated && isUsernameValidated) {
+            setIsFormValidated(true);
+        }
+        else
+            setIsFormValidated(false);
+    }, [isPassword1Validated, isPassword2Validated, isEmailValidated, isFirstNameValidated, isLastNameValidated, isUsernameValidated])
+
+    const checkForUsername = async (check) => {
+        document.getElementById('existing-username').style.display = 'none';
+        const username_input = document.getElementById('username');
+        try{
+            const response = await axios.get('/api/members/check-username/', {params: {username:check}});
+            if (response.data.availability) {
+                validateField(username_input);
+                document.getElementById('existing-username').style.display = 'none';
+                setIsUsernameValidated(true);
+            }
+            else {
+                invalidateField(username_input);
+                document.getElementById('existing-username').style.display = 'block';
+                setIsUsernameValidated(false);
+            }
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+    
+    useEffect(() => {
+        const debouncedFunction = setTimeout(() => {
+            formValidate && username && checkForUsername(username);
+            }, 300);
+          return () => clearTimeout(debouncedFunction);
+    }, [username, formValidate]);
+
+    useEffect(() => {
+        const pass1_input = document.getElementById('password1');
+        const pass2_input = document.getElementById('password2');
+        const fn_input = document.getElementById('first_name');
+        const ln_input = document.getElementById('last_name');
+        const username_input = document.getElementById('username');
+        const email_input = document.getElementById('email');
+
+        if(formValidate) {
+            if(password1.length < 8 || password1.length > 20) {
+                invalidateField(pass1_input);
+                document.getElementById('passwordHelp').classList.add('invalid-feedback');
+                document.getElementById('passwordHelp').style.display = 'block';
+                setIsPassword1Validated(false);
+            } else {
+                validateField(pass1_input);
+                document.getElementById('passwordHelp').classList.remove('invalid-feedback');
+                setIsPassword1Validated(true);
+            }
+            if(password2.length < 8 || password2.length > 20 || password1 !== password2) {
+                invalidateField(pass2_input);
+                document.getElementById('invalid-password2').style.display = 'block';
+                setIsPassword2Validated(false);
+            } else {
+                validateField(pass2_input);
+                document.getElementById('invalid-password2').style.display = 'none';
+                setIsPassword2Validated(true);
+            }
+            if (firstName === '') {
+                invalidateField(fn_input);
+                document.getElementById('invalid-name').style.display = 'block';
+                setIsFirstNameValidated(false);
+            } else {
+                validateField(fn_input);
+                setIsFirstNameValidated(true);
+            }
+            if (lastName === '') {
+                invalidateField(ln_input);
+                document.getElementById('invalid-name').style.display = 'block';
+                setIsLastNameValidated(false);
+            } else {
+                validateField(ln_input);
+                setIsLastNameValidated(true);
+            }
+            if (firstName && lastName) document.getElementById('invalid-name').style.display = 'none';
+            if (username === '') {
+                invalidateField(username_input);
+                document.getElementById('invalid-username').style.display = 'block';
+                setIsUsernameValidated(false);
+            } else {
+                document.getElementById('invalid-username').style.display = 'none';
+            }
+            const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+            if (!emailRegex.test(email)) {
+                invalidateField(email_input);
+                document.getElementById('invalid-email').style.display = 'block';
+                setIsEmailValidated(false);
+            } else {
+                validateField(email_input);
+                document.getElementById('invalid-email').style.display = 'none';
+                setIsEmailValidated(true);
+            }
+        }
+    }, [password1, password2, formValidate, firstName, lastName, username, email]);
 
     return (
         
         <div className="card">
-            {errorMessage && 'Something went wrong'}
-            <form className="p-2">
+            <form className="p-2 needs-validation" id="registration-form" noValidate>
                 <div className="row p-2">
                     <div className="col-auto">
                         <label htmlFor="email" className="col-form-label">
@@ -69,27 +208,48 @@ const RegisterForm = () => {
                         </label>
                     </div>
                     <div className="col-9">
-                        <input type="email" className="form-control" placeholder="Email Address" id="email" onChange={(event) => setEmail(event.target.value)}/>
+                        <input type="email" className="form-control mt-1" placeholder="Email Address" id="email" value={email} onChange={(event) => setEmail(event.target.value)} required/>
+                    </div>
+                    <div className="invalid-feedback" id="invalid-email">
+                        Provide a valid email address
                     </div>
                 </div>
 
                 <div className="row p-2">
-                    <div className="col-auto">
+                    <div className="col-auto d-flex align-items-center">
                         <label htmlFor="username" className="col-form-label">
                             <FaUser className="icon" />
                         </label>
                     </div>
-                    <div className="col-6">
-                        <input type="text" className="form-control" placeholder="Username" id="username" onChange={(event) => setUsername(event.target.value)}/>
+                    <div className="col-10 row">
+                        <div className="col-8 d-flex align-items-center">
+                            <input type="text" className="form-control my-1" placeholder="Username" id="username" value={username} onChange={(event) => setUsername(event.target.value)} required/>
+                        </div>
+                        <div className="col-4 d-flex align-items-center">
+                            <div className="invalid-feedback m-0" id="invalid-username">
+                                Provide a valid username
+                            </div>
+                            <div className="invalid-feedback m-0" id="existing-username">
+                                Username is aleady used
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div className="row p-2">
-                    <div className="col-5">
-                        <input type="text" className="form-control" placeholder="First Name" id="first_name" onChange={(event) => setFirstName(event.target.value)}/>
+                <div className="row p-2 input-group">
+                    <div className="col-auto">
+                        <label className="col-form-label">
+                            <FaIdCard className="icon" />
+                        </label>
                     </div>
                     <div className="col-5">
-                        <input type="text" className="form-control" placeholder="Last Name" id="last_name" onChange={(event) => setlastName(event.target.value)}/>
+                        <input type="text" className="form-control" placeholder="First Name" id="first_name" value={firstName} onChange={(event) => setFirstName(event.target.value)} required/>
+                    </div>
+                    <div className="col-5">
+                        <input type="text" className="form-control" placeholder="Last Name" id="last_name" value={lastName} onChange={(event) => setlastName(event.target.value)} required/>
+                    </div>
+                    <div className="invalid-feedback" id="invalid-name">
+                        Names can't be empty
                     </div>
                 </div>
 
@@ -100,7 +260,7 @@ const RegisterForm = () => {
                         </label>
                     </div>
                     <div className="col-auto">
-                        <input type="password" className="form-control" placeholder="Create a password" id="password1" onChange={(event) => setPassword1(event.target.value)} aria-describedby="passwordHelp"/>
+                        <input type="password" className="form-control" placeholder="Create a password" id="password1" value={password1} onChange={(event) => {setPassword1(event.target.value)}} aria-describedby="passwordHelp" required/>
                     </div>
                     <div id="passwordHelp" className="form-text">Password must be 8-20 characters long</div>
                 </div>
@@ -108,21 +268,33 @@ const RegisterForm = () => {
                 <div className="row p-2">
                     <div className="col-auto">
                         <label htmlFor="password2" className="col-form-label">
-                            <FaLock className="icon" />
+                            <FaKey className="icon" />
                         </label>
                     </div>
                     <div className="col-auto">
-                        <input type="password" className="form-control" placeholder="Confirm password" id="password2" onChange={(event) => setPassword2(event.target.value)} />
+                        <input type="password" className="form-control" placeholder="Confirm password" id="password2" value={password2} onChange={(event) => setPassword2(event.target.value)} required/>
+                    </div>
+                    <div className="invalid-feedback" id="invalid-password2">
+                        Passwords aren't matching!
                     </div>
                 </div>
 
                 <div className="row p-2">
                     <div className="col-auto d-flex align-items-center">
-                        <input type="checkbox" className="form-check-input m-0" id="rememberme" onChange={(event) => {setRememberme(event.target.checked)}} />
+                        <input type="checkbox" className="form-check-input m-0" id="rememberme" value={rememberme} onChange={(event) => {setRememberme(event.target.checked)}} />
                         <label htmlFor="rememberme" className="form-check-label ms-2">Remember me</label>
                     </div>
                     <div className="col-auto ms-3 d-flex align-items-center">
-                    <button type="submit" className="btn btn-primary" onClick={handleRegister}>Register</button>
+                    <button type="submit" className="btn btn-primary" onClick={(event) => {
+                        event.preventDefault();
+                        if (!formValidate) {
+                            handleRegister();
+                            setFormValidate(true);
+                        }
+                        if(isFormValidated) {
+                            handleRegister();
+                        }
+                    }}>Register</button>
                     </div>
                 </div>
                 
--- src/components/SigninForm.jsx
+++ src/components/SigninForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react"
+import { useState, useContext, useEffect } from "react"
 import { FaLock, FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -14,12 +14,40 @@ const SigninForm = () => {
     const [password, setPassword] = useState('');
     const [rememberme, setRememberme] = useState(false);
 
-    const [error, setError] = useState(false);
-    const [errorMessage, setErrorMessage] = useState(null);
-    // const [successMessage, setSuccessMessage] = useState(null);
+    const [errors, setErrors] = useState(null);
 
-    const handleSignin = async (event) => {
-        event.preventDefault();
+    const [formValidate, setFormValidate] = useState(false);
+    const [isFormValidated, setIsFormValidated] = useState(false);
+
+    useEffect(() => {
+        const password_input = document.getElementById('password');
+        const username_input = document.getElementById('username');
+        const handleError = () => {
+            if(errors && errors === 'password') {
+                invalidateField(password_input);
+                document.getElementById('wrong-password').style.display = 'block';
+                setIsPasswordValidated(false);
+            }
+            if(errors && errors === 'username') {
+                invalidateField(username_input);
+                document.getElementById('wrong-username').style.display = 'block';
+                setIsUsernameValidated(false);
+            }
+        }
+        handleError();
+    }, [errors]);
+
+    const clearFields = () => {
+        setUsername('');
+        setPassword('');
+        setRememberme(false);
+        setErrors(null);
+        setFormValidate(false);
+        setIsFormValidated(false);
+    }
+
+    const handleSignin = async () => {
+        setErrors(null);
         const formData = new FormData();
         formData.append('username', username);
         formData.append('password', password);
@@ -27,56 +55,139 @@ const SigninForm = () => {
         
         const response = await axios.post('/api/members/signin/', formData, {headers: { 'X-CSRFToken': csrftoken }});
         if (response.data.success) {
+            clearFields();
             window.bootstrap.Modal.getInstance('#accounts').hide();
             setIsAuthenticated(true);
         }
         else {
-            setError(true);
-            setErrorMessage(response.data.messages || 'Something went wrong');
+            setErrors(response.data.errors);
             setIsAuthenticated(false);
         }
     }
+    
+    const [isPasswordValidated, setIsPasswordValidated] = useState(false);
+    const [isUsernameValidated, setIsUsernameValidated] = useState(false);
+
+    useEffect(() => {
+        if(isPasswordValidated && isUsernameValidated) {
+            setIsFormValidated(true);
+        }
+        else
+            setIsFormValidated(false);
+    }, [isPasswordValidated, isUsernameValidated])
+
+
+    const validateField = (input) => {
+        input.classList.contains('is-invalid') && input.classList.remove('is-invalid');
+    }
+
+    const invalidateField = (input) => {
+        !input.classList.contains('is-invalid') && input.classList.add('is-invalid');
+    }
+
+    useEffect(() => {
+        const password_input = document.getElementById('password');
+        if(formValidate) {
+            if (password === '') {
+                invalidateField(password_input);
+                document.getElementById('wrong-password').style.display = 'none';
+                document.getElementById('invalid-password').style.display = 'block';
+                setIsPasswordValidated(false);
+            } else {
+                validateField(password_input);
+                document.getElementById('wrong-password').style.display = 'none';
+                document.getElementById('invalid-password').style.display = 'none';
+                setIsPasswordValidated(true);
+            }
+        }
+    }, [password, formValidate]);
+
+    useEffect(() => {
+        const username_input = document.getElementById('username');
+        if(formValidate) {
+            if (username === '') {
+                invalidateField(username_input);
+                document.getElementById('wrong-username').style.display = 'none';
+                document.getElementById('invalid-username').style.display = 'block';
+                setIsUsernameValidated(false);
+            } else {
+                validateField(username_input);
+                document.getElementById('wrong-username').style.display = 'none';
+                document.getElementById('invalid-username').style.display = 'none';
+                setIsUsernameValidated(true);
+            }
+        }
+    }, [formValidate, username]);
 
     return (
         <div>
         <div className="card">
-                {error && errorMessage}
-            <form className="p-2">
+            <form className="p-2 needs-validation" id="login-form" noValidate>
+
                 <div className="row p-2">
-                    <div className="col-auto">
+                    <div className="col-auto d-flex align-items-center">
                         <label htmlFor="username" className="col-form-label">
                             <FaUser className="icon" />
                         </label>
                     </div>
-                    <div className="col-auto">
-                        <input type="text" className="form-control" placeholder="Enter your username" id="username" onChange={(event) => setUsername(event.target.value)} />
+                    <div className="col-10 row">
+                        <div className="col-8 d-flex align-items-center">
+                            <input type="text" className="form-control my-1" placeholder="Enter your username" id="username" value={username} onChange={(event) => setUsername(event.target.value)} required/>
+                        </div>
+                        <div className="col-4 d-flex align-items-center">
+                        <div className="invalid-feedback m-0" id="invalid-username">
+                                Enter your username!
+                            </div>
+                            <div className="invalid-feedback m-0" id="wrong-username">
+                                Username doesnot exist!
+                            </div>
+                        </div>
                     </div>
                 </div>
 
                 <div className="row p-2">
-                    <div className="col-auto">
+                    <div className="col-auto d-flex align-items-center">
                         <label htmlFor="password" className="col-form-label">
                             <FaLock className="icon" />
                         </label>
                     </div>
-                    <div className="col-auto">
-                        <input type="password" className="form-control" placeholder="Enter your password" id="password" onChange={(event) => setPassword(event.target.value)} aria-describedby="passwordHelp"/>
+                    <div className="col-10 row">
+                        <div className="col-8 d-flex align-items-center">
+                            <input type="password" className="form-control my-1" placeholder="Enter your password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} aria-describedby="passwordHelp" required/>
+                        </div>
+                        <div className="col-4 d-flex align-items-center">
+                        <div className="invalid-feedback m-0" id="invalid-password">
+                                Enter your password
+                            </div>
+                            <div className="invalid-feedback m-0" id="wrong-password">
+                                Password is incorrect!
+                            </div>
+                        </div>
                     </div>
                     <div id="passwordHelp" className="form-text">Never share your passwords with anyone</div>
                 </div>
 
                 <div className="row p-2">
                     <div className="col-auto d-flex align-items-center">
-                        <input type="checkbox" className="form-check-input m-0" id="rememberme" onChange={(event) => {setRememberme(event.target.checked)}} />
+                        <input type="checkbox" className="form-check-input m-0" id="rememberme" value={rememberme} onChange={(event) => {setRememberme(event.target.checked)}} />
                         <label htmlFor="rememberme" className="form-check-label ms-2">Remember me</label>
                     </div>
                     <div className="col-auto ms-3 d-flex align-items-center">
-                    <button type="submit" className="btn btn-primary" onClick={handleSignin}>Sign in</button>
+                    <button type="submit" className="btn btn-primary" onClick={(event) => {
+                        event.preventDefault();
+                        if(isFormValidated) {
+                            handleSignin();
+                        }
+                        if (!formValidate) {
+                            if(username && password ) handleSignin();
+                            setFormValidate(true);
+                        }
+                    }}>Sign in</button>
                     </div>
                 </div>
 
                 <div className="row p-2">
-                    <Link className="link">Forgot Password?</Link>
+                    <Link className="link" style={{textDecoration:'none'}}>Forgot Password?</Link>
                 </div>
 
             </form>
